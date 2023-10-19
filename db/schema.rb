3f8fd499d29d0224b1495332b200838e81734740
Anderson Freitas <freitaslimaanderson@gmail.com>
2023-10-19 14:18:07 -0300

    Insert new camp in table and create new migrate

--- db/schema.rb
+++ db/schema.rb
@@ -10,13 +10,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_10_19_042802) do
+ActiveRecord::Schema[7.0].define(version: 2023_10_19_163559) do
   create_table "recipes", force: :cascade do |t|
     t.string "name"
     t.text "stuff"
     t.integer "calories"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.text "prepare_mode"
+    t.decimal "cost"
   end
 
 end
