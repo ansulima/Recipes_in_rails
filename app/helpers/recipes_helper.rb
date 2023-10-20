c64eb09ebce884ee25cec18a3cae3e5d8677714c
Anderson Freitas <freitaslimaanderson@gmail.com>
2023-10-19 23:09:58 -0300

    Create helpers

--- app/helpers/recipes_helper.rb
+++ app/helpers/recipes_helper.rb
@@ -1,2 +1,9 @@
 module RecipesHelper
+  def format_calories(recipe)
+    if recipe.light?
+      content_tag(:strong, "Light (menos de 100  calorias)")
+    else
+      recipe.calories
+    end
+  end
 end
